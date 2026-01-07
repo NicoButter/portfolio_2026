7cb559941f91e1131f5d05a3ee219cd1e350b428
Nicolás Butterfield <nicobutter@gmail.com>
2026-01-06 21:58:11 -0300

    feat: replace user profile section with nano editor simulation and enhance styling

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -1,9 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [RouterLink],
   templateUrl: './about.html',
   styleUrl: './about.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
